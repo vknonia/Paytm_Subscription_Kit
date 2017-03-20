cb4bcb9ece1c6d1321d094095747df123f9027af
Paytm-Payments <Mohit.aggarwal@paytm.com>
2017-03-20 15:49:03 +0530

    Subscription Kit PHP

---
+++ PHP/PaytmKitSubscription/SubsRenew.php
@@ -0,0 +1,50 @@
+<?php
+header("Pragma: no-cache");
+header("Cache-Control: no-cache");
+header("Expires: 0");
+
+// following files need to be included
+require_once("./lib/config_paytm.php");
+require_once("./lib/encdec_paytm.php");
+
+$checkSum = "";
+$paramList = array();
+
+$paramList["REQUEST_TYPE"] = 'RENEW_SUBSCRIPTION';
+$paramList["MID"] = PAYTM_MERCHANT_MID;
+$paramList["ORDER_ID"] = "OrderRenewTest00000000001";
+$paramList["SUBS_ID"] = '1002410';
+$paramList["TXN_AMOUNT"] = '40';
+
+$checkSum = getChecksumFromArray($paramList,'');
+$paramList["CHECKSUMHASH"] = $checkSum;
+
+foreach ( $paramList as $key => $value) 
+{
+    $post_items[] = $key . '=' . $value;
+}
+$post_string = implode ('&', $post_items);
+echo $post_string."<br/><br/>";
+
+
+$url = PAYTM_TXN_URL; // where you want to post data
+
+$ch = curl_init();  // initiate curl
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_POST, true);  // tell curl you want to post something
+curl_setopt($ch, CURLOPT_POSTFIELDS,$post_string); // define what you want to post
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // return the output in string format
+$output = curl_exec ($ch); // execute
+$info = curl_getinfo($ch);
+echo $output."<br/><br/>";
+print_r($info)."<br/><br/>";
+
+$data = json_decode($output, true);
+
+
+
+
+
+?>
---
+++ PHP/PaytmKitSubscription/TxnStatus.php
@@ -0,0 +1,36 @@
+<?php
+header("Pragma: no-cache");
+header("Cache-Control: no-cache");
+header("Expires: 0");
+
+// following files need to be included
+require_once("./lib/config_paytm.php");
+require_once("./lib/encdec_paytm.php");
+
+$ORDER_ID = "";
+$requestParamList = array();
+$responseParamList = array();
+
+
+$requestParamList = array("MID" => "Jublia80102820918555" , "ORDERID" => "243a4f59c32a1ab7c0dc");  
+$url = "https://secure.paytm.in/oltp/HANDLER_INTERNAL/TXNSTATUS";
+$data_string = "JsonData=".json_encode($requestParamList);
+
+$ch = curl_init();  // initiate curl
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_POST, 1);  // tell curl you want to post something
+curl_setopt($ch, CURLOPT_POSTFIELDS,$data_string); // define what you want to post
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // return the output in string format
+//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);     
+//curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);    
+//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$output = curl_exec ($ch); // execute
+$info = curl_getinfo($ch);
+
+echo '<br/><br/>';
+print_r($info);
+echo '<br/><br/>';
+echo $output;
+	
+
+?>
